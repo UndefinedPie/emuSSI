735b3c1e712fefcedb4e73fa5ea5d804ff8156d4
v.pirogov <v.pirogov@corp.mail.ru>
2019-02-01 19:31:10 +0300

    add index file

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+module.exports = (RenderTemplate, config) => {
+	const virtualPathRegexp = new RegExp(`<!--#include virtual="\/${config.virtualPath}\/(.*?)" wait="yes"-->`, "gi");
+	const ssiIncludes = RenderTemplate.match(virtualPathRegexp);
+	if (Array.isArray(ssiIncludes)) {
+		ssiIncludes.map(item => {
+			let rbPath = item.split('"')[1].split("?")[0];
+			let id = rbPath.split(`/${config.virtualPath}/`)[1];
+			let fileName = path.join(__dirname, rbPath);
+			let ssiEmptyRegexp = new RegExp(`<!--# if expr="(.)SLOT_${id}(.*?)endif-->`, "gi");
+			let ssiEmptyExpr = RenderTemplate.match(ssiEmptyRegexp);
+			try {
+				fs.accessSync(fileName, fs.constants.R_OK);
+				RenderTemplate = RenderTemplate.replace(
+					item,
+					fs.readFileSync(fileName, "utf8")
+				);
+				if (ssiEmptyExpr[0]) {
+					RenderTemplate = RenderTemplate.replace(ssiEmptyExpr[0], "");
+				}
+			} catch (err) {
+				RenderTemplate = RenderTemplate.replace(item, "");
+				console.error("Not exsit file: " + fileName);
+			}
+		});
+	}
+	return RenderTemplate;
+};
