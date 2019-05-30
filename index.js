1fff537f49b0df51cd94852f9868e9fd5c8018c2
Angmor23 <v.e.pirogov@gmail.com>
2019-05-30 15:29:52 +0300

    refactor: index.js

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 const fs = require("fs");
 const path = require("path");
 const exports = (html, config) => {
-	const virtualPathRegexp = new RegExp(
-		`<!--#include virtual="/${config.virtualPath}/(.*?)" wait="yes"-->`,
-		"gi"
-	);
-	const ssiIncludes = html.match(virtualPathRegexp);
+	const { virtualPath, expr } = config;
+
+	if (!virtualPath && !expr) return;
+
+	const ssiIncludes = html.match(/<!--(.*?)#(.*?)include(.*?)-->/gi);
+
 	if (Array.isArray(ssiIncludes)) {
-		ssiIncludes.map(item => {
-			let rbPath = item.split('"')[1].split("?")[0];
-			let id = rbPath.split(`/${config.virtualPath}/`)[1];
-			let fileName = path.join(__dirname).split("node_modules")[0] + rbPath;
-			let ssiEmptyRegexp = new RegExp(
-				`<!--# if expr="(.)SLOT_${id}(.*?)endif-->`,
-				"gi"
+		ssiIncludes.forEach(ssiInc => {
+			const virtualRegExp = new RegExp(
+				`virtual=("|')${virtualPath}(.*?)?(.*?)("|')`,
+				`gi`
 			);
-			let ssiEmptyExpr = html.match(ssiEmptyRegexp);
+
+			const ssiIncMatch = ssiInc.match(virtualRegExp);
+			const ssiIncName = Array.isArray(ssiIncMatch) && ssiIncMatch[2];
+			const ssiIncPath = virtualPath && ssiIncName && virtualPath + virtualPath;
+			const fileName =
+				path.join(__dirname).split("node_modules")[0] + ssiIncPath;
+			const ssiExpr = new RegExp(
+				`<!--# if expr="${expr.replace("${ID}", fileName)}(.*?)endif-->`,
+				`gi`
+			);
+			const ssiExprMatch = html.match(ssiExpr);
+
 			try {
 				fs.accessSync(fileName, fs.constants.R_OK);
-				html = html.replace(item, fs.readFileSync(fileName, "utf8"));
-				if (ssiEmptyExpr[0]) {
-					html = html.replace(ssiEmptyExpr[0], "");
+				html = html.replace(ssiInc, fs.readFileSync(fileName, "utf8"));
+				if (ssiExprMatch && ssiExprMatch[0]) {
+					html = html.replace(ssiExprMatch[0], "");
 				}
 			} catch (err) {
-				html = html.replace(item, "");
+				html = html.replace(ssiInc, "");
 			}
 		});
 	}
